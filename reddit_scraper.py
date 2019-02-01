a0c3e3c85a294231ed272219a5670d580b2d52ea
anders <anders@edelbolillie.dk>
2019-02-01 14:22:18 +0100

    New stories, text_url, and error handling

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -38,6 +38,7 @@ def submissioninfo(submission):
     sub_data['created'] = convtime(submission.created_utc)
     sub_data['num_comments'] = submission.num_comments
     sub_data['url'] = submission.permalink
+    sub_data['text_url'] = submission.url
     sub_data['upvotes'] = submission.score
     sub_data['is_video'] = submission.is_video
     return sub_data
@@ -55,8 +56,8 @@ def userinfo(user):
         user_data['gold_status'] = user.is_gold
         user_data['is_employee'] = user.is_employee
         user_data['has_verified_email'] = user.has_verified_email if user.has_verified_email is not None else False
-    except prawcore.exceptions.NotFound:
-        return {}
+    except Exception:
+        return user_data
     return user_data
 
 def subredditinfo(subreddit, subreddit_id):
@@ -82,20 +83,23 @@ def commentsinfo(comments):
             time.sleep(1)
     for comment in comments.list(): #flatten all nested comments
         data = {}
-        data['comment_id'] = comment.id
-        data['text'] = comment.body
-        is_deleted = False
-        if ( data['text'] == '[deleted]'):
-            is_deleted = True
-        data['is_deleted'] = is_deleted
-        data['created'] = convtime(comment.created_utc)
-        data['is_submitter'] = comment.is_submitter
-        data['submission_id'] = comment.link_id
-        data['parent_id'] = comment.parent_id
-        data['comment_url'] = comment.permalink
-        data['upvotes'] = comment.score
-        data['replies'] = comment.replies.__len__()
-        data['user'] = userinfo(comment.author)
+        try:
+            data['comment_id'] = comment.id
+            data['text'] = comment.body
+            is_deleted = False
+            if ( data['text'] == '[deleted]'):
+                is_deleted = True
+            data['is_deleted'] = is_deleted
+            data['created'] = convtime(comment.created_utc)
+            data['is_submitter'] = comment.is_submitter
+            data['submission_id'] = comment.link_id
+            data['parent_id'] = comment.parent_id
+            data['comment_url'] = comment.permalink
+            data['upvotes'] = comment.score
+            data['replies'] = comment.replies.__len__()
+            data['user'] = userinfo(comment.author)
+        except Exception:
+            print("Comment fail")
 
         comments_data.append(data)
     return comments_data
