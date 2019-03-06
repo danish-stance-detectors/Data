3970907bede572763d147c2514f4166b3cb8e1c0
Emilos_de_carlos <erem@itu.dk>
2019-03-06 14:22:26 +0100

    added link to nice stack overflow post for later use

--- TwitterScraper/scraper/twitter_scraper.py
+++ TwitterScraper/scraper/twitter_scraper.py
@@ -4,7 +4,7 @@ import time
 
 # Loads twitter credentials
 twitter_cred = dict()
-with open('twitter_credentials.json') as f:
+with open('twitter_archive_credentials.json') as f:
     secret_info = json.load(f)
     twitter_cred["CONSUMER_KEY"] = secret_info["CONSUMER_KEY"]
     twitter_cred["CONSUMER_SECRET"] = secret_info["CONSUMER_SECRET"]
@@ -16,30 +16,34 @@ auth = tweepy.OAuthHandler(twitter_cred["CONSUMER_KEY"], twitter_cred["CONSUMER_
 auth.set_access_token(twitter_cred["ACCESS_KEY"], twitter_cred["ACCESS_SECRET"])
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(), wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-# search = "ulv"
+search = "#kimwall"
+
+# look into this: https://stackoverflow.com/questions/22469713/managing-tweepy-api-search
 
 # #wait_on_rate_limit=True to get a lot
 # # applies search term
-# submarine_tweets = api.search(q=search, rpp=100, lang="da")
+submarine_tweets = api.search(q=search, rpp=100, lang="da")
+
+print(submarine_tweets)
 
 # # dump data into file
 # with open('./data/tweets_' + search + '2' + '.json', 'w') as f:
 #     json.dump(submarine_tweets, f)
 
-replies=[] 
-non_bmp_map = dict()
+# replies=[] 
+# non_bmp_map = dict()
 
-tweet_id = 990676636071874565
-user_name = 'BjarkeCharlie'
+# tweet_id = 990676636071874565
+# user_name = 'BjarkeCharlie'
 
-for full_tweets in tweepy.Cursor(api.user_timeline,screen_name=user_name,timeout=999999).items(10):
-  for tweet in tweepy.Cursor(api.search,q='to:'+user_name, since_id=tweet_id,timeout=999999).items(1000):
-    if hasattr(tweet, 'in_reply_to_status_id_str'):
-      if (tweet.in_reply_to_status_id_str==full_tweets.id_str):
-        replies.append(tweet.text)
-  print("Tweet :",full_tweets.text.translate(non_bmp_map))
-  for elements in replies:
-       print("Replies :",elements)
+# for full_tweets in tweepy.Cursor(api.user_timeline,screen_name=user_name,timeout=999999).items(10):
+#   for tweet in tweepy.Cursor(api.search,q='to:'+user_name, since_id=tweet_id,timeout=999999).items(1000):
+#     if hasattr(tweet, 'in_reply_to_status_id_str'):
+#       if (tweet.in_reply_to_status_id_str==full_tweets.id_str):
+#         replies.append(tweet.text)
+#   print("Tweet :",full_tweets.text.translate(non_bmp_map))
+#   for elements in replies:
+#        print("Replies :",elements)
 
 # for tweet in submarine_tweets:
 #     print(tweet.text)
