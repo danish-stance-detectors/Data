7794272a3253f524ef60b21e10039a81b546c218
Emilos_de_carlos <erem@itu.dk>
2019-03-06 14:02:55 +0100

    old changes to twitter scraper

--- TwitterScraper/scraper/twitter_scraper.py
+++ TwitterScraper/scraper/twitter_scraper.py
@@ -14,17 +14,32 @@ with open('twitter_credentials.json') as f:
 # sets up tweepy api
 auth = tweepy.OAuthHandler(twitter_cred["CONSUMER_KEY"], twitter_cred["CONSUMER_SECRET"])
 auth.set_access_token(twitter_cred["ACCESS_KEY"], twitter_cred["ACCESS_SECRET"])
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(), wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-search = "#ulvesagen"
+# search = "ulv"
 
-#wait_on_rate_limit=True to get a lot
-# applies search term
-submarine_tweets = api.search(q=search, count=100, lang="da")
+# #wait_on_rate_limit=True to get a lot
+# # applies search term
+# submarine_tweets = api.search(q=search, rpp=100, lang="da")
 
-# dump data into file
-with open('tweets_' + search + '.json', 'w') as f:
-    json.dump(submarine_tweets, f)
+# # dump data into file
+# with open('./data/tweets_' + search + '2' + '.json', 'w') as f:
+#     json.dump(submarine_tweets, f)
+
+replies=[] 
+non_bmp_map = dict()
+
+tweet_id = 990676636071874565
+user_name = 'BjarkeCharlie'
+
+for full_tweets in tweepy.Cursor(api.user_timeline,screen_name=user_name,timeout=999999).items(10):
+  for tweet in tweepy.Cursor(api.search,q='to:'+user_name, since_id=tweet_id,timeout=999999).items(1000):
+    if hasattr(tweet, 'in_reply_to_status_id_str'):
+      if (tweet.in_reply_to_status_id_str==full_tweets.id_str):
+        replies.append(tweet.text)
+  print("Tweet :",full_tweets.text.translate(non_bmp_map))
+  for elements in replies:
+       print("Replies :",elements)
 
 # for tweet in submarine_tweets:
 #     print(tweet.text)
