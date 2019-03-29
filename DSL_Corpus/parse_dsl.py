c8bf11af20631eda6e292c4e3686778eb18f27a9
anders <anders@edelbolillie.dk>
2019-03-29 11:12:30 +0100

    Switch for enabling POS tags

--- DSL_Corpus/parse_dsl.py
+++ DSL_Corpus/parse_dsl.py
@@ -10,7 +10,7 @@ def find_files(path, filter):
     
     return file_paths
 
-def parse_file(file_path):
+def parse_file(file_path, pos_tags=False):
     start_tag = "<s"
     end_tag = "</s>"
     current_sentence = ""
@@ -19,17 +19,20 @@ def parse_file(file_path):
     with open(file_path, "r", encoding="utf8") as txt_file:
         for line in txt_file.readlines():
             if line.startswith(end_tag):
-                s = current_sentence.strip() + '\t' + current_pos_tags
+                s = current_sentence.strip() + '\t'
+                if pos_tags:
+                    s += current_pos_tags
+                    current_pos_tags = ""
                 sentences.append(s)
                 current_sentence = ""
-                current_pos_tags = ""
             elif not line.startswith(start_tag):
                 instance = line.split("\t")
                 first_word = instance[0]
-                epos_tag = instance[5]
-                pos_class = epos_tag.split(':')[0]
                 current_sentence += (first_word + " ")
-                current_pos_tags += (pos_class + " ")
+                if pos_tags:
+                    epos_tag = instance[5]
+                    pos_class = epos_tag.split(':')[0]
+                    current_pos_tags += (pos_class + " ")
 
     return sentences
 
