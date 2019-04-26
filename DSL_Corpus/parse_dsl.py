0141520b5b35e97675b45f358e35002ff8b12925
anders <anders@edelbolillie.dk>
2019-04-26 08:40:51 +0200

    Ignore weird punctuation

--- DSL_Corpus/parse_dsl.py
+++ DSL_Corpus/parse_dsl.py
@@ -1,5 +1,6 @@
 import os, fnmatch, re
 import argparse
+from nltk import word_tokenize
 
 punctuation = re.compile('[^a-zA-ZæøåÆØÅ0-9]')
 
@@ -7,7 +8,7 @@ punctuation = re.compile('[^a-zA-ZæøåÆØÅ0-9]')
 dsl_data_folder = "./dsl_corpus_data/"
 
 parser = argparse.ArgumentParser(description='Parse DSL data')
-parser.add_argument('-p', '--pos', dest='pos', action='store_true', help='Include POS tags')
+parser.add_argument('-p', '--pos', dest='pos', action='store_true', default=False, help='Include POS tags')
 args = parser.parse_args()
 
 def find_files(path, filter):
@@ -22,27 +23,26 @@ def parse_file(file_path, pos_tags=False):
     start_tag = "<s"
     end_tag = "</s>"
     current_sentence = ""
-    current_pos_tags = ""
+    # current_pos_tags = ""
     sentences = []
     with open(file_path, "r", encoding="utf8") as txt_file:
         for line in txt_file.readlines():
             if line.startswith(end_tag):
-                s = current_sentence.strip()
-                if pos_tags:
-                    s += '\t' + current_pos_tags
-                    current_pos_tags = ""
-                sentences.append(s)
+                clean_text = punctuation.sub(' ', current_sentence.strip())
+                tokens = word_tokenize(clean_text, language='danish')
+                # if pos_tags:
+                    # s += '\t' + current_pos_tags
+                    # current_pos_tags = ""
+                sentences.append(tokens)
                 current_sentence = ""
             elif not line.startswith(start_tag):
                 instance = line.split("\t")
                 first_word = instance[0]
-                if len(first_word) == 1 and punctuation.match(first_word):
-                    continue
                 current_sentence += (first_word + " ")
-                if pos_tags:
-                    epos_tag = instance[5]
-                    pos_class = epos_tag.split(':')[0]
-                    current_pos_tags += (pos_class + " ")
+                # if pos_tags:
+                    # epos_tag = instance[5]
+                    # pos_class = epos_tag.split(':')[0]
+                    # current_pos_tags += (pos_class + " ")
 
     return sentences
 
@@ -56,7 +56,9 @@ def save_all_files(file_path_list, output_path):
             sentences = parse_file(file_path, args.pos)
 
             for sentence in sentences:
-                output_file.write("%s\n" % sentence)
+                for token in sentence:
+                    output_file.write(token + ' ')
+                output_file.write("\n")
 
 files = find_files(dsl_data_folder, "*.txt")
 
