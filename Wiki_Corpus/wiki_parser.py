d6a24f6291da333f0a81cf2c518b7114bca85702
anders <anders@edelbolillie.dk>
2019-04-28 19:14:47 +0200

    Change parsing

--- Wiki_Corpus/wiki_parser.py
+++ Wiki_Corpus/wiki_parser.py
@@ -1,18 +1,38 @@
 import re, sys
+from nltk import word_tokenize, sent_tokenize
 
 wiki_corpus = 'da_wiki_text.txt'
 out_file = 'wiki_sentences.txt'
+sec_num = re.compile(r'(^\[\[\d+\]\]\n)')
+punctuation = re.compile('[^a-zA-ZæøåÆØÅ0-9]')
 
 with open(out_file, 'w+', encoding='utf8') as outfile, open(wiki_corpus, 'r', encoding='utf8') as corpus:
     sections = corpus.read().split('\n\n\n\n')
     line_count = 0
     for section in sections:
-        for line in section.split('\n'):
-            if(re.fullmatch(r'^\[\[\d+\]\]$', line) or len(line)==1):
-                continue
-            line = re.sub("[^a-zA-ZæøåÆØÅ0-9]", " ", line)
-            outfile.write(line + '\n')
+        sentences = sent_tokenize(section, language='danish')
+        # sentences_merged = []
+        # for sentence in sentences:
+        #     sentence = sec_num.sub('', sentence)
+        #     if not sentence[0].isupper():
+        #         last = sentences_merged[-1]
+        #         del sentences_merged[-1]
+        #         sentences_merged.append(' '.join([last, sentence]))
+        #     else:
+        #         sentences_merged.append(sentence)
+
+        sentence_tokens = [word_tokenize(sec_num.sub('', t), language='danish') for t in sentences]
+        for tokens in sentence_tokens:
             line_count += 1
             if line_count % 100000 == 0:
                 print("Parsed %d lines" % line_count)
+            tokens_clean = []
+            for token in tokens:
+                if token and not punctuation.match(token):
+                    tokens_clean.append(token)
+            if not tokens_clean or len(tokens_clean) == 1:
+                continue
+            for t in tokens_clean:
+                outfile.write(t + ' ')
+            outfile.write('\n')
 
